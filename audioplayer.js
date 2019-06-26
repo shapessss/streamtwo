4140b3bb4e5eabb70e1dc7e1c85e6d224b3c0d9d
Jordan Delgado <jordelquino@gmail.com>
2019-06-25 21:21:12 -0500

    adding new shit

--- audioplayer.js
+++ audioplayer.js
@@ -1,8 +1,21 @@
-function aud_play_pause() {
-  var myAudio = document.getElementById("myTune");
-  if (myAudio.paused) {
-    myAudio.play();
-  } else {
-    myAudio.pause();
-  }
-}
+var audio = new Audio("https://stream.streamaudio.de:8000/internet-public-radio");
+
+$('#play-pause-button').on("click",function(){
+  if($(this).hasClass('fa-play'))
+   {
+     $(this).removeClass('fa-play');
+     $(this).addClass('fa-pause');
+     audio.play();
+   }
+  else
+   {
+     $(this).removeClass('fa-pause');
+     $(this).addClass('fa-play');
+     audio.pause();
+   }
+});
+
+audio.onended = function() {
+     $("#play-pause-button").removeClass('fa-pause');
+     $("#play-pause-button").addClass('fa-play');
+};
